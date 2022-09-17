29eb8f3d68e5f2cc68c7364dfa4ee35c28dd2d0e
Bagirka <bruna07@mail.ru>
2022-09-17 22:38:51 +0500

    Task_001

---
+++ Task/Task_001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число a! ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b! ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b > a)
+{
+    Console.WriteLine("5 меньшее число! ");
+}
+if(a < b)
+{
+    Console.WriteLine("7 большее число! ");
+}
