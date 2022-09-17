47032e647d695d6bed90fabfea64874d3f4329ac
Bagirka <bruna07@mail.ru>
2022-09-18 00:02:22 +0500

    Task_001

--- Task/Task_001/Program.cs
+++ Task/Task_001/Program.cs
@@ -3,11 +3,9 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число b! ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-if(b > a)
-{
-    Console.WriteLine("5 меньшее число! ");
-}
-if(a < b)
-{
-    Console.WriteLine("7 большее число! ");
-}
+int[] numbers = { a, b };
+
+int min = numbers.Min();
+int max = numbers.Max();
+Console.WriteLine($"Min: {min}");
+Console.WriteLine($"Max: {max}");
