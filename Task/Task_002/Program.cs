a70cf6613c8de5595aaec84ccfece1905707efc2
Bagirka <bruna07@mail.ru>
2022-09-18 11:56:46 +0500

    Task_002

---
+++ Task/Task_002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число a! ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b! ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c! ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = { a, b, c };
+
+int max = numbers.Max();
+Console.WriteLine($"Max: {max}");
+
